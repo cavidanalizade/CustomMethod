d1f66e450440da6fa8dc02f4c15d522403874d55
unknown <bd7q9887u@code.edu.az>
2023-10-10 18:24:52 +0400

    Practice

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomString customString = new CustomString();
+            Console.WriteLine(CustomString.Remove("xjsnjhijoklj ", 4));
+            string[] words = { "saaslm", "sdldsfld", "sagol" };
+            Console.WriteLine(CustomString.Join('/'  , words));
+
+        }
+    }
+}
